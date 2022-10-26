55954d208fb86e551f9c427e74e1af70a786dff8
Baris @ 1815 <baris@1815.nl>
2022-10-26 07:48:07 +0200

    chore: apply WordPress Coding Standards

--- update-your-footer-wp.php
+++ update-your-footer-wp.php
@@ -13,18 +13,18 @@
 */
 
 // prevent direct access
-defined( 'ABSPATH' ) or die( "Restricted Access!" );
+defined( 'ABSPATH' ) or die( 'Restricted Access!' );
 
-function update_your_footer($atts) {
+function update_your_footer( $atts ) {
 
     // wp data
-    $year = date('Y');
-    $site_url = get_bloginfo('url');
-    $site_name = get_bloginfo('name');
+    $year      = date( 'Y' );
+    $site_url  = get_bloginfo( 'url' );
+    $site_name = get_bloginfo( 'name' );
 
     // output data
     return 'Copyright &copy; ' . esc_html( $year ) . ' <a href="' . esc_url( $site_url ) . '">' . esc_html( $site_name ) . '</a>, All Rights Reserved.';
 }
 
 // wordpress hook
-add_shortcode('uyf_wp', 'update_your_footer');
+add_shortcode( 'uyf_wp', 'update_your_footer' );
